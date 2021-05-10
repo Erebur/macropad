1b8cfacd7ac7f6398eb2bcde63d3b7a8c6741355
Erebur <simon.web999@gmail.com>
2021-05-10 21:03:14 +0200

    eingabe geändert

--- src/Keypress.java
+++ src/Keypress.java
@@ -9,6 +9,7 @@ public class Keypress {
     //im prinzip main
     public static void keypress(SerialPort comPort) throws AWTException, InterruptedException {
         int oldInput = 0;
+        int oldInputausgabe = 0;
 
         while (Macropadmain.getPreset() != 0) {
             Scanner s     = new Scanner(comPort.getInputStream());
@@ -18,41 +19,62 @@ public class Keypress {
             Macropadmain.waiting(comPort);
 
             //saves input in var
-            var input = s.nextInt();
+            var input = Integer.parseInt(s.nextLine());
 
             //input in console ausgeben
 
-            if (oldInput != input) System.out.println();
-            oldInput = input;
+            if (oldInputausgabe != input) System.out.println();
+            oldInputausgabe = input;
             System.out.print(input);
 
             //sucht sich den Hexcode aus den presets
             int[] key = Presets.getKey(input);
 
+            //TODO its a mess pls fix --> mit button halten pls
             if (key.length == 1 ){
                 //einfach das preset ändern
                 switch (key[ 0 ]) {
                     case 0:
-                        Macropadmain.presetswichdialog();
+                        if (oldInput != input){
+                            Macropadmain.presetswichdialog();
+                            oldInput = input;
+                        }else{
+                            oldInput = 0 ;
+                        }
                         break;
                     //cases with a key pressed
                     case KeyEvent.VK_NUM_LOCK:
-                        Macropadmain.switchnumlock();
+                        if (oldInput != input){
+                            Macropadmain.switchnumlock();
+                        }
                     default:
-                        robot.keyPress(key[ 0 ]);
-                        robot.keyRelease(key[ 0 ]);
+                        if (oldInput != input){
+                            robot.keyPress(key[ 0 ]);
+                            oldInput = input;
+                        }else{
+                            robot.keyRelease(key[ 0 ]);
+                            oldInput = 0 ;
+                        }
                         break;
                 }
             }else {
-                //drückt alle tasten
-                for (int j : key) {
-                    robot.keyPress(j);
+                if (oldInput != input){
+                    //drückt alle tasten
+                    for (int j : key) {
+                        robot.keyPress(j);
+                        oldInput = input;
+                    }
+                }else {
+                    //lässt alle tasten los
+                    for (int j : key) {
+                        robot.keyRelease(j);
+                        oldInput = 0 ;
+                    }
                 }
-                //lässt alle tasten los
-                for (int j : key) {
-                    robot.keyRelease(j);
-                }
+
+
             }
+
 
 
         }
