3df35ec5f074231e292eab1198bfc607308baa2f
Erebur <simon.web999@gmail.com>
2021-05-16 00:36:26 +0200

    preset 4(numlck aus)überarbeitet (7 fehlt)

--- src/Presets.java
+++ src/Presets.java
@@ -36,14 +36,17 @@ public class Presets {
             put(13 , new ArrayList<>(){{add(KeyEvent.VK_CONTROL);add(KeyEvent.VK_ALT);add(KeyEvent.VK_PAGE_UP);}});}});
         //preset 2 wasd
         presets.add(new HashMap<>() {{
+            //reihe 1
             put(2 , new ArrayList<>(){{add(KeyEvent.VK_A);}});
             put(3 , new ArrayList<>(){{add(KeyEvent.VK_S);}});
             put(4 , new ArrayList<>(){{add(KeyEvent.VK_D);}});
             put(5 , new ArrayList<>(){{add(KeyEvent.VK_F);}});
+            //reihe 2
             put(6 , new ArrayList<>(){{add(KeyEvent.VK_Q);}});
             put(7 , new ArrayList<>(){{add(KeyEvent.VK_W);}});
             put(8 , new ArrayList<>(){{add(KeyEvent.VK_E);}});
             put(9 , new ArrayList<>(){{add(KeyEvent.VK_R);}});
+            //reihe 3
             put(10 , new ArrayList<>(){{add(0);}});
             put(11 , new ArrayList<>(){{add(KeyEvent.VK_1);}});
             put(12 , new ArrayList<>(){{add(KeyEvent.VK_2);}});
@@ -67,19 +70,19 @@ public class Presets {
             put(13 , new ArrayList<>(){{add(KeyEvent.VK_NUM_LOCK);}});}});
         //preset 4 wenn numlock aus
         presets.add(new HashMap<>() {{
-            put(2 , new ArrayList<>(){{add(KeyEvent.VK_LEFT);}});
+            put(2 , new ArrayList<>(){{add(KeyEvent.VK_END);}});
             put(3 , new ArrayList<>(){{add(KeyEvent.VK_DOWN);}});
-            put(4 , new ArrayList<>(){{add(KeyEvent.VK_RIGHT);}});
-            put(5 , new ArrayList<>(){{add(KeyEvent.VK_NUMPAD7);}});
+            put(4 , new ArrayList<>(){{add(KeyEvent.VK_PAGE_DOWN);}});
+            put(5 , new ArrayList<>(){{add(KeyEvent.VK_ENTER);}});
             //reihe 2
-            put(6 , new ArrayList<>(){{add(KeyEvent.VK_END);}});
-            put(7 , new ArrayList<>(){{add(KeyEvent.VK_UP);}});
-            put(8 , new ArrayList<>(){{add(KeyEvent.VK_HOME);}});
-            put(9 , new ArrayList<>(){{add(KeyEvent.VK_NUMPAD8);}});
+            put(6 , new ArrayList<>(){{add(KeyEvent.VK_LEFT);}});
+            put(7 , new ArrayList<>(){{add(KeyEvent.VK_NUMPAD5);}});//Kein plan was das machen soll
+            put(8 , new ArrayList<>(){{add(KeyEvent.VK_RIGHT);}});
+            put(9 , new ArrayList<>(){{add(0);}});
             //reihe 3
-            put(10 , new ArrayList<>(){{add(0);}});
-            put(11 , new ArrayList<>(){{add(KeyEvent.VK_DELETE);}});
-            put(12 , new ArrayList<>(){{add(KeyEvent.VK_ENTER);}});
+            put(10 , new ArrayList<>(){{add(KeyEvent.VK_HOME);}});
+            put(11 , new ArrayList<>(){{add(KeyEvent.VK_UP);}});
+            put(12 , new ArrayList<>(){{add(KeyEvent.VK_PAGE_UP);}});
             put(13 , new ArrayList<>(){{add(KeyEvent.VK_NUM_LOCK);}});}});
         //preset 5 music
         presets.add(new HashMap<>() {{
